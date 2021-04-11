cd6a96d192bb40d1446c423c07895ba044f69496
vidhirkaup <vidhir.kaup@gmail.com>
2021-04-10 20:17:00 -0400

    added dev tools

--- src/main/java/com/vlabs/hw/alpha/controller/PingController.java
+++ src/main/java/com/vlabs/hw/alpha/controller/PingController.java
@@ -1,15 +1,21 @@
 package com.vlabs.hw.alpha.controller;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RestController
 public class PingController {
 
     @GetMapping("/ping")
     public String ping(){
-        return String.format("hw-alpha service pinged @ [%s]", LocalDateTime.now());
+        String msg = String.format("pinged [%s] @ [%s]", "hw-alpha", LocalDateTime.now());
+
+        log.info(msg);
+        return msg;
     }
 }
